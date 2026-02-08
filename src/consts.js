506cc8aa59e81e5817f9e73472e81ebe21c695b8
Jaiden Magnan <jaidenmagnan@gmail.com>
2026-02-08 16:55:58 -0500

    fix: j money not a link

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "jmoney";
+export const SITE_TITLE = "j money";
 export const SITE_DESCRIPTION = "jmoney's personal website.";
 export const SITE_URL = "https://jmoney.world";
